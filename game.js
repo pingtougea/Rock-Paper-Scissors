2a05a3d84d125aada757653b1991598357781b0a
pingtougea <1465164225@qq.com>
2025-06-14 12:27:23 +0800

    play the game

--- game.js
+++ game.js
@@ -6,7 +6,27 @@ function getHumanChoice(){
   let userc = prompt("请输入rock,paper,scissors 其中的一种")
   console.log(userc)//
 }
+function playRound(humanChoice,computerChoice){
+  humanChoice = humanChoice.toLowerCase()
+  console.log(humanChoice)
+  if((humanChoice === 'rock' && computerChoice === 'scissors')||(humanChoice === 'scissors' && computerChoice === 'paper')||(humanChoice === 'paper' && computerChoice === 'rock')){
+    humanScore++
+     console.log(`You lose! ${humanChoice} beats ${computerChoice}`)
+  }
+  else if (humanChoice === computerChoice)
+  {
+    console.log("It's a tie!")
+  }
+  else{
+    computerScore++
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`)
+  }
+   
+}
 let arr = ['rock','paper','scissors']
-getComputerChoice()//
-getHumanChoice()//
+let humanScore = 0
+let computerScore = 0 
+const humanSelection = getHumanChoice()//
+const computerSelection = getComputerChoice()//
 
+playRound(humanSelection, computerSelection)
